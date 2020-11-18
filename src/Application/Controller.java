976001a5069afd0dae6b156e28417d115e1fc2bc
meokisama <hoangdinhsang123@gmail.com>
2020-11-18 22:40:43 +0700

    Big update and Clean code

--- src/Application/Controller.java
+++ src/Application/Controller.java
@@ -3,12 +3,14 @@ package Application;
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
+import org.controlsfx.control.textfield.TextFields;
 import javafx.event.ActionEvent;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,15 +19,19 @@ public class Controller implements Initializable {
     @FXML
     private AnchorPane anchorPane;
     @FXML
-    private Pane SearchPane, ShowPane, GooglePane, AddPane;
+    private Pane SearchPane, ShowPane, GooglePane, AddPane, AboutPane;
     @FXML
-    private JFXButton SearchButton, ShowButton, AddButton, GoogleButton;
+    private JFXButton SearchButton, ShowButton, AddButton, GoogleButton, AboutButton;
+    @FXML
+    private TextField InputSearch;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
 
+    //Change Pane with the selected Button
+    //Start
     @FXML
     private void handleButtonAction(ActionEvent event) {
         if (event.getSource() == SearchButton) {
@@ -36,13 +42,29 @@ public class Controller implements Initializable {
             AddPane.toFront();
         } else if (event.getSource() == GoogleButton) {
             GooglePane.toFront();
+        } else if (event.getSource() == AboutButton) {
+            AboutPane.toFront();
         }
     }
+    //End.
 
+    //Auto complete when search a word
+    //Start
+    @FXML
+    private void InputTextField(ActionEvent event) {
+        String[] input = {"Hi", "Hello"};
+        TextFields.bindAutoCompletion(InputSearch, input);
+    }
+    //End.
+
+
+    //Close the program when click Exit icon
+    //Start
     @FXML
     private void CloseButton(MouseEvent event) {
         Stage stage = (Stage) anchorPane.getScene().getWindow();
         stage.close();
     }
+    //End.
 
 }
--- src/Application/Main.java
+++ src/Application/Main.java
@@ -6,7 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.scene.paint.Color;
 
 public class Main extends Application {
 
@@ -16,22 +15,23 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
-        Scene scene = new Scene(root);
         primaryStage.setTitle("EVDictionary");
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-        scene.setFill(Color.TRANSPARENT);
+        primaryStage.initStyle(StageStyle.UNDECORATED); //Hide the TitleBar of OS
 
+
+        //Make program can be dragged without TitleBar
+        //Start
         root.setOnMousePressed(event -> {
             x = event.getSceneX();
             y = event.getSceneY();
         });
-
         root.setOnMouseDragged(event -> {
             primaryStage.setX(event.getScreenX() - x);
             primaryStage.setY(event.getScreenY() - y);
         });
+        //End.
 
-        primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
